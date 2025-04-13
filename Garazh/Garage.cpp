c42974b5481cd6d0c37460d7ad0055db35f5c4d6
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-13 23:40:40 +0300

    main

--- Garazh/Garage.cpp
+++ Garazh/Garage.cpp
@@ -1,20 +1,33 @@
 #include "Garage.h"
 
 Vehicle::Vehicle() : mark{"unknow"}, mileage{0}{}
+Vehicle::Vehicle(std::string _mark, int _mileage) : Vehicle()
+{
+	mark = _mark;
+	mileage = _mileage;
+}
 
 Car::Car() : is_active{ false } {}
+Car::Car(bool _is_active) : Car()
+{
+	is_active = _is_active;
+}
+
 
 Bus::Bus() : capacity{0} {}
-
+Bus::Bus(int _capacity) : Bus()
+{
+	capacity = _capacity;
+}
 
 void Vehicle::info() const
 {
-	std::cout << "Mark: " << mark << ", mileage" << mileage << std::endl;
+	std::cout << "Mark: " << mark << ", mileage: " << mileage << std::endl;
 }
 
 void Car::info() const
 {
-	std::cout << "Is active" << is_active << std::endl;
+	std::cout << "Is active: " << is_active << std::endl;
 }
 
 void Bus::info() const
@@ -22,21 +35,10 @@ void Bus::info() const
 	std::cout << "Capacity: " << capacity << std::endl;
 }
 
-void Garage::park(Vehicle* vehicle)
+void Garage::park(const std::shared_ptr<Vehicle>& vehicle)
 {
 	vehicles.push_back(vehicle);
 }
-
-
-// Снизу приемный гараж
-
-//Garage::~Garage()
-//{
-//	for (int i = 0; i < vehicles.size(); i++)
-//	{
-//		delete vehicles.at(i);
-//	}
-//}
 
 void Garage::printVehicle() const
 {
@@ -45,3 +47,13 @@ void Garage::printVehicle() const
 		vehicles.at(i)->info();
 	}
 }
+
+// Приемный гараж
+
+//Garage::~Garage()
+//{
+//	for (int i = 0; i < vehicles.size(); i++)
+//	{
+//		delete vehicles.at(i);
+//	}
+//}
--- Garazh/Main.cpp
+++ Garazh/Main.cpp
@@ -4,12 +4,16 @@ int main()
 {
 	Garage garage;
 
-	Vehicle* bus = new Bus();
-	Vehicle* car = new Car();
+	std::shared_ptr<Vehicle> bus = std::make_shared<Bus>(100);
+	std::shared_ptr<Vehicle> car = std::make_shared<Car>(true);
+
+	std::shared_ptr<Vehicle> bike = std::make_shared<Vehicle>("Bike", 120);
 
 	garage.park(bus);
 	garage.park(car);
+	garage.park(bike);
 
 	garage.printVehicle();
+
 	return 0;
 }
